9d64b48c20e4b364b6a8776326bbb5a326e08577
Youhua Xu <yhxu@nao.cas.cn>
2016-11-02 21:07:34 +0800

    Update function_omega2.cpp
    bug-fix

--- src/function_omega2.cpp
+++ src/function_omega2.cpp
@@ -4,6 +4,6 @@ int rips::function_omega2( int m, int n, double& omega2_mn ){
     omega2_mn = (this->D/this->mu)
                 *( pow(m*_pi_/this->l1,4)
                 + 2.0*pow(m*_pi_/this->l1,2)*pow(n*_pi_/this->l2,2)
-                + pow(m*_pi_/this->l1,4) );
+                + pow(n*_pi_/this->l2,4) );
     return 0;
 }
